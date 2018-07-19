1b9e0e880103cfa59c6aea97274240e523a4878b
Jay <lay.jecavalier@gmail.com>
2018-07-19 12:25:52 -0600

    Improved the input sanitizer. Works like a charm now!

--- Assets/_Scripts/Spanner/Editor/MortalSystem/MortalSettingsEditor.cs
+++ Assets/_Scripts/Spanner/Editor/MortalSystem/MortalSettingsEditor.cs
@@ -4,34 +4,66 @@ using UnityEditor;
 namespace Spanner {
     [CustomEditor(typeof(MortalSettings))]
     public class MortalSettingsEditor : Editor {
+
+        private struct SerializedProperties {
+            public SerializedProperty randomizeHealth;
+            public SerializedProperty maxHealthCap;
+            public SerializedProperty minHealthCap;
+            public SerializedProperty maxStartingHealth;
+            public SerializedProperty minStartingHealth;
+            public SerializedProperty maxHealth;
+            public SerializedProperty startingHealth;
+        }
+
+        private SerializedProperties _properties;
+
+        private void OnEnable() {
+            _properties.randomizeHealth = serializedObject.FindProperty("randomizeHealth");
+            _properties.maxHealthCap = serializedObject.FindProperty("maxHealthCap");
+            _properties.minHealthCap = serializedObject.FindProperty("minHealthCap");
+            _properties.maxStartingHealth = serializedObject.FindProperty("maxStartingHealth");
+            _properties.minStartingHealth = serializedObject.FindProperty("minStartingHealth");
+            _properties.maxHealth = serializedObject.FindProperty("maxHealth");
+            _properties.startingHealth = serializedObject.FindProperty("startingHealth");
+        }
+
         public override void OnInspectorGUI() {
-            // Get the Mortal Settings instance
-            MortalSettings settings = (MortalSettings) target;
+            // Get new property values from the serializedObject
+            serializedObject.Update();
 
             // Always draw the checkbox for 'Randomize Health'
-            SerializedProperty randomizeHealth = serializedObject.FindProperty("randomizeHealth");
-            EditorGUILayout.PropertyField(randomizeHealth);
+            EditorGUILayout.PropertyField(_properties.randomizeHealth);
 
             // Change the inspector layout depending on the value of 'Randomize Health'
-            if (settings.randomizeHealth) {
-                SerializedProperty maxHealthCap = serializedObject.FindProperty("maxHealthCap");
-                SerializedProperty minHealthCap = serializedObject.FindProperty("minHealthCap");
-                SerializedProperty maxStartingHealth = serializedObject.FindProperty("maxStartingHealth");
-                SerializedProperty minStartingHealth = serializedObject.FindProperty("minStartingHealth");
-
-                EditorGUILayout.PropertyField(minHealthCap);
-                EditorGUILayout.PropertyField(maxHealthCap);
-                EditorGUILayout.PropertyField(minStartingHealth);
-                EditorGUILayout.PropertyField(maxStartingHealth);
+            if (_properties.randomizeHealth.boolValue) {
+                EditorGUILayout.PropertyField(_properties.minHealthCap);
+                EditorGUILayout.PropertyField(_properties.maxHealthCap);
+                EditorGUILayout.PropertyField(_properties.minStartingHealth);
+                EditorGUILayout.PropertyField(_properties.maxStartingHealth);
             } else {
-                SerializedProperty maxHealth = serializedObject.FindProperty("maxHealth");
-                SerializedProperty startingHealth = serializedObject.FindProperty("startingHealth");
-
-                EditorGUILayout.PropertyField(maxHealth);
-                EditorGUILayout.PropertyField(startingHealth);
+                EditorGUILayout.PropertyField(_properties.maxHealth);
+                EditorGUILayout.PropertyField(_properties.startingHealth);
             }
 
+            SanitizeInputs();
+
+            // Apply changes made from the inspector to the object
             serializedObject.ApplyModifiedProperties();
+        }
+
+        /// <summary>
+        /// Forces the values input by the inspector to be within an appropriate range
+        /// </summary>
+        private void SanitizeInputs() {
+            if (_properties.randomizeHealth.boolValue) {
+                _properties.minHealthCap.intValue = Mathf.Clamp(_properties.minHealthCap.intValue, 1, int.MaxValue);
+                _properties.maxHealthCap.intValue = Mathf.Clamp(_properties.maxHealthCap.intValue, _properties.minHealthCap.intValue, int.MaxValue);
+                _properties.minStartingHealth.intValue = Mathf.Clamp(_properties.minStartingHealth.intValue, 1, _properties.maxHealthCap.intValue);
+                _properties.maxStartingHealth.intValue = Mathf.Clamp(_properties.maxStartingHealth.intValue, _properties.minStartingHealth.intValue, _properties.maxHealthCap.intValue);
+            } else {
+                _properties.maxHealth.intValue = Mathf.Clamp(_properties.maxHealth.intValue, 1, int.MaxValue);
+                _properties.startingHealth.intValue = Mathf.Clamp(_properties.startingHealth.intValue, 1, _properties.maxHealth.intValue);
+            }
         }
     }
 }
