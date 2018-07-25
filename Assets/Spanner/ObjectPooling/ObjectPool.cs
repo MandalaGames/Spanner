ce73d3f2001c63c2c54d766b00af57073f2a36ff
Jay <lay.jecavalier@gmail.com>
2018-07-25 16:25:58 -0600

    Fixed bug

--- Assets/Spanner/ObjectPooling/ObjectPool.cs
+++ Assets/Spanner/ObjectPooling/ObjectPool.cs
@@ -2,45 +2,47 @@ using UnityEngine;
 using System;
 using System.Collections.Generic;
 
-[Serializable]
-public class ObjectPool {
-    public GameObject prefabToPool;
-    public int numberToPool;
-    public bool startEnabled;
-    public string poolID;
-    
-    [SerializeField, HideInInspector]
-    private List<GameObject> _instances;
-    [SerializeField, HideInInspector]
-    private Transform _parentTransform;
+namespace Spanner {
+    [Serializable]
+    public class ObjectPool {
+        public GameObject prefabToPool;
+        public int numberToPool;
+        public bool startEnabled;
+        public string poolID;
 
-    public void Clear(Transform master) {
-        _instances = new List<GameObject>();
-        if (_parentTransform != null) {
-            UnityEngine.Object.DestroyImmediate(_parentTransform.gameObject);
+        [SerializeField, HideInInspector]
+        private List<GameObject> _instances;
+        [SerializeField, HideInInspector]
+        private Transform _parentTransform;
+
+        public void Clear(Transform master) {
+            _instances = new List<GameObject>();
+            if (_parentTransform != null) {
+                UnityEngine.Object.DestroyImmediate(_parentTransform.gameObject);
+            }
+
+            _parentTransform = (new GameObject(poolID.Equals("") ? prefabToPool.name : poolID)).transform;
+            _parentTransform.parent = master;
         }
 
-        _parentTransform = (new GameObject(poolID.Equals("") ? prefabToPool.name : poolID)).transform;
-        _parentTransform.parent = master;
-    }
-
-    public void Initialize() {
-        for (int i = 0; i < numberToPool; i++) {
-            GameObject instance = UnityEngine.Object.Instantiate(prefabToPool, _parentTransform);
-            instance.SetActive(startEnabled);
-            _instances.Add(instance);
+        public void Initialize() {
+            for (int i = 0; i < numberToPool; i++) {
+                GameObject instance = UnityEngine.Object.Instantiate(prefabToPool, _parentTransform);
+                instance.SetActive(startEnabled);
+                _instances.Add(instance);
+            }
         }
-    }
 
-    public GameObject Get() {
-        GameObject instance = _instances.Find(go => !go.activeInHierarchy);
-        if (instance != null) {
-            instance.SetActive(true);
+        public GameObject Get() {
+            GameObject instance = _instances.Find(go => !go.activeInHierarchy);
+            if (instance != null) {
+                instance.SetActive(true);
+            }
+            return instance;
         }
-        return instance;
-    }
 
-    public void Test() {
-        Debug.Log(_instances.ToArray().Length);
+        public void Test() {
+            Debug.Log(_instances.ToArray().Length);
+        }
     }
 }
